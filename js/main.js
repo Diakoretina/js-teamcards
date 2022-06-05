5257a7aeeb0de9a6f9d63c687a7dff81b5df1578
Diakoretina <francesco.tammaro2000@gmail.com>
2022-06-06 00:05:23 +0200

    Add JS

--- js/main.js
+++ js/main.js
@@ -30,3 +30,45 @@ const teams = [
     role: "Graphic Designer",
   },
 ];
+
+//Inizializzazione delle variabili
+
+let container = document.querySelector(".cards__container");
+let inputName = document.getElementById("name");
+let inputRole = document.getElementById("role");
+let inputImg = document.getElementById("img");
+
+//Il bottone crea il nuovo oggetto
+
+let buttonAddMember = document.querySelector(".add-btn");
+buttonAddMember.addEventListener("click", (e) => {
+  e.preventDefault();
+  displayImages();
+  addObject(inputName, inputRole, inputImg);
+});
+
+//La funzione svolge i processi per la creazione dell'oggetto
+
+function addObject(name, role, img) {
+  let newObj = {
+    img: img.value,
+    name: name.value,
+    role: role.value,
+  };
+  teams.push(newObj);
+}
+
+displayImages();
+
+//Il div viene aggiunto nel HTML
+
+function displayImages() {
+  container.innerHTML = "";
+  for (i = 0; i < teams.length; i++) {
+    container.innerHTML += `<div>
+        <img src="${teams[i].img}" alt="${teams[i].name}">
+        <h4>${teams[i].name}</h4>
+        <p>${teams[i].role}</p>
+        </div>`;
+  }
+}
