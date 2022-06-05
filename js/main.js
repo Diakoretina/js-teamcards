697723920a955c68e7c6b44c55de9811b9518362
Diakoretina <francesco.tammaro2000@gmail.com>
2022-06-05 13:31:11 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const teams = [
+  {
+    img: "wayne-barnett-founder-ceo.jpg",
+    name: "Wayne Barnet",
+    role: "Founder & CEO",
+  },
+  {
+    img: "maria-medoro-chief-editor.jpg",
+    name: "Maria Medoro",
+    role: "Chief Editor",
+  },
+  {
+    img: "walter-gordon-office-manager.jpg",
+    name: "Walter Gordon",
+    role: "Office Manager",
+  },
+  {
+    img: "angela-proietti-social-media-manager.jpg",
+    name: "Angela Proietti",
+    role: "Social Media Manager",
+  },
+  {
+    img: "scott-estrada-developer.jpg",
+    name: "Scott Estrada",
+    role: "Developer",
+  },
+  {
+    img: "barbara-gabrielli-graphic-designer.jpg",
+    name: "Barbara Gabrielli",
+    role: "Graphic Designer",
+  },
+];
